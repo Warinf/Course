36c9ed8c46ba9823656449e1a331b4f08175366c
Warinf <38905087+Warinf@users.noreply.github.com>
2018-05-09 16:15:04 +0200

    Main Project file

--- Main_Project.py
+++ Main_Project.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
-df = pd.read_csv('test.csv', sep=',')
+df = pd.read_csv('test.csv', index_col=0, sep=',')
+x = df.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x)
+df = pd.DataFrame(x_scaled)
+
 print(df)
-
-
-
+plt.plot(df)
+plt.show()
 
