0e306c5afea2e7e197ec69faa9e17fed0b00d253
Warinf <38905087+Warinf@users.noreply.github.com>
2018-05-22 11:10:47 +0200

    Melted

--- Main_Project.py
+++ Main_Project.py
@@ -1,14 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
+import numpy as py
 
-df = pd.read_csv('test.csv', index_col=0, sep=',')
-x = df.values #returns a numpy array
-min_max_scaler = preprocessing.MinMaxScaler()
-x_scaled = min_max_scaler.fit_transform(x)
-df = pd.DataFrame(x_scaled)
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+df = pd.read_csv('test.csv', header=0, sep=',' )
+
+colnames = list(df.columns)
+print(colnames)
+colnames.pop(0)
+for i in colnames:
+
+    df[i]= (df[i] - df[i].min()) / (df[i].max() - df[i].min())
+
+
 
 print(df)
-plt.plot(df)
+df.plot(x='Temperature')
 plt.show()
+
+y = input('Enter temperature')
+d1 = df[:int(y)]
+
+melt = pd.melt(d1, id_vars=["Temperature"], var_name="Exp", value_name="Intensity")
+print(melt)
+
+un=melt['Intensity']
+
+x = 0.5
+print(melt.ix[(melt.Intensity-x).abs().argsort()[:1]])
 
