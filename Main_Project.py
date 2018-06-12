e4d1cf22c27fe2139dbca0a98189440729b45971
Warinf <38905087+Warinf@users.noreply.github.com>
2018-06-12 14:01:17 +0200

    Final

--- Main_Project.py
+++ Main_Project.py
@@ -27,16 +27,16 @@ a = input('Second temperature')
 d2 = d1[d1['Temperature'] <= int(a)]
 
 colnames2 = list(d2.columns)
-for i in colnames2:
-    df[i] = (df[i] - df[i].min()) / (df[i].max() - df[i].min())
+colnames2.pop(0)
+for col in colnames2:
+    d2[col] = (d2[col] - d2[col].min()) / (d2[col].max() - d2[col].min())
 main = d2.plot(x='Temperature')
 line = plt.axhline(y=0.5, color='black', linestyle='-')
-
+print(d2)
 
 plt.show()
 d3 = d2.set_index(['Temperature'])
-d4= d3- 0.5
-
+d4 = d3- 0.5
 d5 = d4.abs()
 d6 = d5.idxmin()
 
